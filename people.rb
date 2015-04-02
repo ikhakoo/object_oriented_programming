664ec592b07f77070091f05807186c3ce11cb7eb
ikhakoo <ikhakoo@gmail.com>
2015-04-02 13:40:10 -0400

    step 3 everything in ruby is an object

--- people.rb
+++ people.rb
@@ -5,7 +5,12 @@ class Student
 end
 
 class Instructor
+	def teach
+		puts "Everything in Ruby is an object!"
+	end
 end
 
 student = Student.new
+teacher = Instructor.new
 puts student.learn
+puts teacher.teach 
