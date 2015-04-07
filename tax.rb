3f9e76b484f116c0e6b1073ba64b216818899503
ikhakoo <ikhakoo@gmail.com>
2015-04-07 14:45:15 -0400

    fixed print out error

--- tax.rb
+++ tax.rb
@@ -13,6 +13,13 @@ class Tax
 		@item_quantity = item_quantity
 		@total_taxes = 0
 		@total_cost = 0
+		add_tax
+	end
+
+	def to_s
+			puts "Item: #{@item_type} Cost: $" "#{@item_cost}"
+			puts "Sales Taxes: $" "#{@total_taxes}"
+			puts "Total Cost: $" "#{@total_cost}"
 	end
 
 	def add_tax
@@ -20,32 +27,31 @@ class Tax
 			if @item_type.include?("imported")
 				@total_taxes = 0
 				@total_cost = @item_cost.to_i * IMPORT_TAX
+				to_s
 			else
 				@total_taxes = 0 
 				@total_cost = @item_cost
+				to_s
 			end
 		else
 			if @item_type.include?("imported")
 				@total_taxes = ((@item_cost.to_i * IMPORT_TAX) * SALES_TAX) - (@item_cost.to_i * IMPORT_TAX)
 				@total_cost = (@item_cost.to_i * IMPORT_TAX) * SALES_TAX
+				to_s
 			else
 				@total_taxes = (@item_cost.to_i * SALES_TAX) - @item_cost.to_i
 				@total_cost = @item_cost.to_i * SALES_TAX
+				to_s
 			end
 
 		end
-		 	 
 
 	end
 
-	def to_s
-			"Item: #{@item_type} Cost: $" "#{@item_cost}"
-			"Sales Taxes: $" "#{@total_taxes}"
-			"Total Cost: $" "#{@total_cost}"
-	end
+	
 
 end
 
 my_tax = Tax.new("1", "books", "12.49")
-my_tax.to_s
+my_tax
 
